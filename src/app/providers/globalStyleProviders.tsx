9a08fa2b5c4753bd03ab7cd275072c3228960ad3
muhammad-stack <142173430+muhammad-stack@users.noreply.github.com>
2024-03-11 11:00:01 +0500

    first commit

---
+++ src/app/providers/globalStyleProviders.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function globalStyleProviders() {
+  return <div>globalStyleProviders</div>;
+}
+
+export default globalStyleProviders;
